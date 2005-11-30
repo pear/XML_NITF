71cb47fb578ef86731d40de17daaa3fe20865b5c
Patrick O'Lone <polone@php.net>
2005-11-30 17:53:42 +0000

    Added support for META tags in the HEAD section.
    git-svn-id: http://svn.php.net/repository/pear/packages/XML_NITF/trunk@201719 c90b9560-bf6c-de11-be94-00142212c4b1

--- NITF.php
+++ NITF.php
@@ -69,6 +69,14 @@ require_once ('XML/Parser.php');
 class XML_NITF extends XML_Parser
 {
     // {{{ properties
+    
+	/**
+	 * Meta tag properties retrieved from document head section
+	 * @see getMetaData()
+	 * @var array
+	 * @access private
+	 */
+	var $m_kMeta = array();
 
     /**
      * Document Metadata
@@ -248,6 +256,19 @@ class XML_NITF extends XML_Parser
         return $this->m_kDocData;
     }
 
+    // }}}
+    // {{{ getMetaData()
+    
+	/**
+	 * Retrieve meta data from the NITF file
+	 * @return array Returns an array of key/value pairs from the meta section
+	 * @access public
+	 */
+	function getMetaData()
+	{
+		return $this->m_kMeta;
+	}
+	
     // }}}
     // {{{ getPubData()
 
@@ -615,6 +636,13 @@ class XML_NITF extends XML_Parser
             case 'REVISION-HISTORY' :
                 array_push($this->m_akRevisions, array_change_key_case($kAttrib, CASE_LOWER));
                 break;
+				
+			case 'META':
+				if (!empty($kAttrib['NAME']) && isset($kAttrib['CONTENT'])) {
+					$sName = strtolower($kAttrib['NAME']);
+					$this->m_kMeta[$sName] = $kAttrib['CONTENT'];
+				}
+				break;
 
         }
 
